73c544911b7f12b390ca8abd72c017a339092902
peter <petermsouzajr@gmail.com>
2023-12-09 15:03:42 -0800

    deleted example files

--- cypress-example/cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-const myNpmPackage = require('your-npm-package-name');
-
-module.exports = (on, config) => {
-  // Pass the custom configuration to your npm package
-  myNpmPackage.setConfig(config.env.myPackageConfig);
-
-  // ... other plugin code ...
-};
