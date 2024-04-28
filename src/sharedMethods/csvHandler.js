8c2306fc19c992ca2dddc4d0a75140eefb44c532
Peter Souza <petermsouzajr@gmail.com>
2024-04-27 18:35:55 -0700

    Main js typecheck (#58)
    * add type checking
    
    * added csv dupport
    
    * added readme details

---
+++ src/sharedMethods/csvHandler.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * Converts a 2D array into a CSV string format.
+ * @param {Array<Array<string>>} data - The 2D array to be converted into CSV format.
+ * @returns {string} The CSV string.
+ */
+const arrayToCSV = (data) => {
+  return data
+    .map((row) =>
+      row
+        .map(String)
+        .map((v) => v.replace(/"/g, '""')) // Using replace with a global regex
+        .map((v) => `"${v}"`)
+        .join(',')
+    )
+    .join('\n');
+};
+
+/**
+ * Saves CSV data to a file in the Cypress downloads directory.
+ * @param {Array<Array<string>>} my2DArray - The 2D array to convert to CSV and save.
+ */
+export const saveCSV = (my2DArray) => {
+  const csvData = arrayToCSV(my2DArray);
+  const downloadsPath = path.join('cypress', 'downloads');
+  const filePath = path.join(downloadsPath, 'dailyReport.csv');
+
+  if (!fs.existsSync(downloadsPath)) {
+    fs.mkdirSync(downloadsPath);
+  }
+
+  fs.writeFileSync(filePath, csvData, 'utf8');
+  console.log(`CSV file has been saved successfully to ${filePath}`);
+};
+
+// // Example usage
+// const my2DArray = [
+//     ['Header1', 'Header2', 'Header3'],
+//     ['Row1 Col1', 'Row1 Col2', 'Row1 Col3'],
+//     ['Row2 Col1', 'Row2 Col2', 'Row2 Col3']
+// ];
+// const csvData = arrayToCSV(my2DArray);
+// saveCSV(csvData);
--- src/sharedMethods/dailyReportHandler.js
+++ src/sharedMethods/dailyReportHandler.js
@@ -23,15 +23,19 @@ import {
   setTextWrappingToClip,
 } from './styles.js';
 import { TEST_DATA } from '../../constants.js';
+import { saveCSV } from './csvHandler.js';
 
 /**
  * Handles the creation and population of a daily report.
- * It builds the payload, creates a new tab for the report, processes the header with formulas,
- * and writes the header and body to the sheet.
+ * Depending on the specified format, it may generate a standard report or a CSV formatted report.
+ * The function builds the payload, creates a new tab for the report, processes the header with formulas,
+ * and writes the header and body to the sheet. The output format is determined by the 'format' parameter.
+ *
  * @async
  * @function handleDailyReport
+ * @param {string|undefined} format - Specifies the output format ('csv' for CSV format; undefined for standard format).
  */
-export const handleDailyReport = async () => {
+export const handleDailyReport = async (format) => {
   try {
     const todaysTitle = getTodaysFormattedDate();
     // Use path.join to create a relative path to the JSON file
@@ -41,86 +45,99 @@ export const handleDailyReport = async () => {
     const dataSet = await loadJSON(jsonFilePath);
 
     const fullDailyPayload = await buildDailyPayload(dataSet);
-    await createNewTab(todaysTitle);
+    if (format === 'csv') {
+      const fullCSVPayload = [
+        // Gets the last element of the headerPayload array, which is the oclumn titles
+        fullDailyPayload.headerPayload[
+          fullDailyPayload.headerPayload.length - 1
+        ],
+        ...fullDailyPayload.bodyPayload,
+      ];
+      saveCSV(fullCSVPayload);
+    } else {
+      await createNewTab(todaysTitle);
+      const destinationTabId = await getTabIdFromTitle(todaysTitle);
+      const headerRowIndex = fullDailyPayload.headerPayload.length + 1;
+      const bodyRowCount = fullDailyPayload.bodyPayload.length;
+      const totalNumberOfRows = headerRowIndex + bodyRowCount - 1;
 
-    const destinationTabId = await getTabIdFromTitle(todaysTitle);
-    const headerRowIndex = fullDailyPayload.headerPayload.length + 1;
-    const bodyRowCount = fullDailyPayload.bodyPayload.length;
-    const totalNumberOfRows = headerRowIndex + bodyRowCount - 1;
+      await processHeaderWithFormulas(
+        fullDailyPayload.headerPayload,
+        headerRowIndex,
+        totalNumberOfRows,
+        bodyRowCount
+      );
 
-    await processHeaderWithFormulas(
-      fullDailyPayload.headerPayload,
-      headerRowIndex,
-      totalNumberOfRows,
-      bodyRowCount
-    );
+      const rowMergePayload = createMergeQueries(
+        fullDailyPayload.bodyPayload,
+        headerRowIndex - 1,
+        destinationTabId
+      );
 
-    const rowMergePayload = createMergeQueries(
-      fullDailyPayload.bodyPayload,
-      headerRowIndex - 1,
-      destinationTabId
-    );
+      const failedPayload = createConditionalFormattingPayload(
+        destinationTabId,
+        fullDailyPayload,
+        'failed'
+      );
+      const passedPayload = createConditionalFormattingPayload(
+        destinationTabId,
+        fullDailyPayload,
+        'passed'
+      );
 
-    const failedPayload = createConditionalFormattingPayload(
-      destinationTabId,
-      fullDailyPayload,
-      'failed'
-    );
-    const passedPayload = createConditionalFormattingPayload(
-      destinationTabId,
-      fullDailyPayload,
-      'passed'
-    );
+      const combinedPayload = {
+        requests: [...failedPayload.requests, ...passedPayload.requests],
+      };
 
-    const combinedPayload = {
-      requests: [...failedPayload.requests, ...passedPayload.requests],
-    };
+      const frozenRowPayload = await freezeRowsInSheet(
+        destinationTabId,
+        headerRowIndex - 1
+      );
 
-    const frozenRowPayload = await freezeRowsInSheet(
-      destinationTabId,
-      headerRowIndex - 1
-    );
+      const colorStylesPayload = await buildColorStylesPayload(
+        destinationTabId,
+        fullDailyPayload
+      );
 
-    const colorStylesPayload = await buildColorStylesPayload(
-      destinationTabId,
-      fullDailyPayload
-    );
+      const rowHeightPayload = await buildRowHeightPayload(
+        destinationTabId,
+        fullDailyPayload
+      ); // For example, to set the height of the first 100 rows to 21 pixels
 
-    const rowHeightPayload = await buildRowHeightPayload(
-      destinationTabId,
-      fullDailyPayload
-    ); // For example, to set the height of the first 100 rows to 21 pixels
+      const textStyle = await BuildTextStyles(
+        destinationTabId,
+        fullDailyPayload
+      );
+      const wrapStrategyPayload = await setTextWrappingToClip(
+        destinationTabId,
+        fullDailyPayload
+      );
 
-    const textStyle = await BuildTextStyles(destinationTabId, fullDailyPayload);
-    const wrapStrategyPayload = await setTextWrappingToClip(
-      destinationTabId,
-      fullDailyPayload
-    );
+      const textAlignmentPayload = await createTextAlignmentPayload(
+        destinationTabId,
+        fullDailyPayload
+      );
 
-    const textAlignmentPayload = await createTextAlignmentPayload(
-      destinationTabId,
-      fullDailyPayload
-    );
+      const columnWidthPayload = await setColumnWidths(
+        destinationTabId,
+        fullDailyPayload
+      );
 
-    const columnWidthPayload = await setColumnWidths(
-      destinationTabId,
-      fullDailyPayload
-    );
+      await writeToSheet(todaysTitle, fullDailyPayload.headerPayload);
+      await writeToSheet(todaysTitle, fullDailyPayload.bodyPayload);
+      await writeToSheet(todaysTitle, fullDailyPayload.footerPayload);
+      await sendGridStyle(destinationTabId, fullDailyPayload);
+      await batchUpdateMasterSheet(combinedPayload);
+      await batchUpdateMasterSheet(frozenRowPayload);
+      await batchUpdateMasterSheet(colorStylesPayload);
+      await batchUpdateMasterSheet(rowHeightPayload);
+      await batchUpdateMasterSheet(textStyle);
+      await batchUpdateMasterSheet(wrapStrategyPayload);
+      await batchUpdateMasterSheet(textAlignmentPayload);
+      await batchUpdateMasterSheet(columnWidthPayload);
 
-    await writeToSheet(todaysTitle, fullDailyPayload.headerPayload);
-    await writeToSheet(todaysTitle, fullDailyPayload.bodyPayload);
-    await writeToSheet(todaysTitle, fullDailyPayload.footerPayload);
-    await sendGridStyle(destinationTabId, fullDailyPayload);
-    await batchUpdateMasterSheet(combinedPayload);
-    await batchUpdateMasterSheet(frozenRowPayload);
-    await batchUpdateMasterSheet(colorStylesPayload);
-    await batchUpdateMasterSheet(rowHeightPayload);
-    await batchUpdateMasterSheet(textStyle);
-    await batchUpdateMasterSheet(wrapStrategyPayload);
-    await batchUpdateMasterSheet(textAlignmentPayload);
-    await batchUpdateMasterSheet(columnWidthPayload);
-
-    await batchUpdateMasterSheet(rowMergePayload);
+      await batchUpdateMasterSheet(rowMergePayload);
+    }
   } catch (error) {
     console.error('Error in handleDailyReport:', error);
     throw error; // Depending on the context you might want to handle the error differently
