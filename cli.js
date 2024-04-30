556307139aa99d35bf8e3e21baed5c9f914ded18
Peter Souza <petermsouzajr@gmail.com>
2024-04-29 21:49:36 -0700

    updated docs (#62)
    * updated docs
    
    * updated docs

--- cli.js
+++ cli.js
@@ -14,35 +14,54 @@ async function run() {
   const isCSV = args.includes('--csv'); // Checks if the CSV output is requested
   const allowDuplicate = args.includes('--duplicate'); // Checks if duplication is permitted
 
+  if (args.includes('--help')) {
+    console.log(`
+      Usage:
+        cy-shadow-report [command] [options]
+        cy-shadow-report     Generate the daily report or monthly summary.
+
+      Commands:
+        todays-report        Generate today's report.
+        monthly-summary      Generate a summary for the current month.
+  
+      Options:
+        --csv                Output the report in CSV format.
+        --duplicate          Allow duplicate reports to be created.
+  
+      For more details, visit the documentation link: https://github.com/petermsouzajr/cy-shadow-report
+    `);
+    process.exit(0);
+  }
+
   try {
     switch (command) {
-    case 'todays-report':
-      /**
+      case 'todays-report':
+        /**
          * Handles the generation of the daily report.
          * @param {Object} options - Contains options specifying output format and duplication permission.
          * @property {boolean} options.csv - If true, outputs in CSV format.
          * @property {boolean} options.duplicate - If true, allows creating a duplicate report for the day.
          */
-      await handleDailyReport({ csv: isCSV, duplicate: allowDuplicate });
-      break;
-    case 'monthly-summary':
-      /**
+        await handleDailyReport({ csv: isCSV, duplicate: allowDuplicate });
+        break;
+      case 'monthly-summary':
+        /**
          * Handles the generation of the monthly summary.
          * @param {Object} options - Contains options specifying output format and duplication permission.
          * @property {boolean} options.csv - If true, outputs in CSV format.
          * @property {boolean} options.duplicate - If true, allows creating a duplicate summary for the month.
          */
-      await handleSummary({ csv: isCSV, duplicate: allowDuplicate });
-      break;
-    default:
-      /**
+        await handleSummary({ csv: isCSV, duplicate: allowDuplicate });
+        break;
+      default:
+        /**
          * Handles the of default command.
          * @param {Object} options - Contains options specifying output format and duplication permission.
          * @property {boolean} options.csv - If true, outputs in CSV format.
          * @property {boolean} options.duplicate - If true, allows creating a duplicate summary for the month.
          */
-      await main({ csv: isCSV, duplicate: allowDuplicate });
-      break;
+        await main({ csv: isCSV, duplicate: allowDuplicate });
+        break;
     }
   } catch (error) {
     // Logs any errors that occur during the execution of the commands
