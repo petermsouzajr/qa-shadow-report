f4824f0008146505e042b35e81b0696acc9dea40
peter <petermsouzajr@gmail.com>
2024-05-05 18:56:36 -0700

    updated readme

--- src/dailyReportMethods/dataExtractionUtilities.js
+++ src/dailyReportMethods/dataExtractionUtilities.js
@@ -1,32 +1,29 @@
-import { TEST_PURPOSES_AVAILABLE } from '../../constants.js';
+import { TEST_CATEGORIES_AVAILABLE } from '../../constants.js';
 
 /**
  * Asynchronously extracts the area from a full file path string, excluding any segments
  * that are listed in the array of available test target types.
  *
  * @param {string} fullFile The full file path from which to extract the area.
- * @param {Array<string>} testTargetsAvailable The array of available test target types to exclude from the area.
+ * @param {Array<string>} testTypesAvailable The array of available test target types to exclude from the area.
  * @returns {Promise<string>} A promise that resolves to the extracted area as a string.
  * @throws {Error} If the input parameters are not valid.
  */
-export const extractAreaFromFullFile = async (
-  fullFile,
-  testTargetsAvailable
-) => {
+export const extractAreaFromFullFile = async (fullFile, testTypesAvailable) => {
   if (typeof fullFile !== 'string') {
     throw new Error('The "fullFile" argument must be a string.');
   }
 
-  if (!Array.isArray(testTargetsAvailable)) {
+  if (!Array.isArray(testTypesAvailable)) {
     throw new Error(
-      'The "testTargetsAvailable" argument must be an array of strings.'
+      'The "testTypesAvailable" argument must be an array of strings.'
     );
   }
 
   const area = fullFile
     .split('/')
     .slice(2, -1) // Assumes the area is between the second and the last segment of the path
-    .filter((segment) => !testTargetsAvailable.includes(segment))
+    .filter((segment) => !testTypesAvailable.includes(segment))
     .join('/');
 
   return area; // The function only returns the area, spec and type were not defined in the original snippet
@@ -36,21 +33,21 @@ export const extractAreaFromFullFile = async (
  * Extracts the category from the test title based on available purpose types.
  *
  * @param {Object} test - An object with title and fullTitle properties representing the test.
- * @param {Array<string>} testPurposesAvailable - An array of strings representing available test purpose types.
+ * @param {Array<string>} testCategoriesAvailable - An array of strings representing available test purpose types.
  * @returns {string} - The extracted category.
  * @throws {TypeError} If the input test is not an object or if the required properties are missing.
- * @throws {TypeError} If the testPurposesAvailable is not an array.
+ * @throws {TypeError} If the testCategoriesAvailable is not an array.
  */
-export const extractCategoryFromTest = (test, testPurposesAvailable) => {
+export const extractCategoryFromTest = (test, testCategoriesAvailable) => {
   if (typeof test !== 'object' || test === null || !test.fullTitle) {
     throw new TypeError(
       'The "test" argument must be an object with a "fullTitle" property.'
     );
   }
 
-  if (!Array.isArray(testPurposesAvailable)) {
+  if (!Array.isArray(testCategoriesAvailable)) {
     throw new TypeError(
-      'The "testPurposesAvailable" argument must be an array of strings.'
+      'The "testCategoriesAvailable" argument must be an array of strings.'
     );
   }
 
@@ -60,7 +57,7 @@ export const extractCategoryFromTest = (test, testPurposesAvailable) => {
   if (categoryMatches) {
     for (let match of categoryMatches) {
       const extractedCategory = match.replace(/[[\]]/g, '');
-      if (testPurposesAvailable.includes(extractedCategory)) {
+      if (testCategoriesAvailable.includes(extractedCategory)) {
         category = extractedCategory;
         break; // Stop at the first matching category
       }
@@ -104,13 +101,13 @@ export const extractTeamNameFromTest = (test) => {
     );
   }
 
-  const testPurposes = TEST_PURPOSES_AVAILABLE();
+  const testCategories = TEST_CATEGORIES_AVAILABLE();
   // Convert the array to a regex string
-  const testPurposesRegex = testPurposes.join('|');
+  const testCategoriesRegex = testCategories.join('|');
 
   // This regex matches any string within brackets that doesn't start with 'C' followed by numbers
   const teamNameMatch = test.fullTitle.match(
-    new RegExp(`\\[((?!${testPurposesRegex}|C\\d+).+?)\\]`)
+    new RegExp(`\\[((?!${testCategoriesRegex}|C\\d+).+?)\\]`)
   );
   return teamNameMatch ? teamNameMatch[1].trim() : '';
 };
@@ -138,55 +135,44 @@ export const extractTestNameFromFullTitle = (fullTitle = '') => {
 };
 
 /**
- * Extracts the TestRail ID from a test's title.
+ * Extracts the Test Case ID from a test's title.
  *
- * @param {Object} test - The test object containing the title property with a TestRail ID.
- * @returns {string|null} - The extracted TestRail ID or null if not present.
+ * @param {Object} test - The test object containing the title property with a Test Case ID.
+ * @returns {string|null} - The extracted Test Case ID or null if not present.
  * @throws {TypeError} If the input 'test' is not an object with a string 'title' property.
  */
-export const extracttestrailIdFromTest = (test) => {
-  const testPurposesTypes = TEST_PURPOSES_AVAILABLE();
-  const testRailIdMatch = test.title.match(/\[C[^\]]+\]/);
-
+export const extractManualTestCaseIdFromTest = (test) => {
   if (!test || typeof test.title !== 'string') {
     throw new TypeError(
       'The "test" argument must be an object with a "title" property of type string.'
     );
   }
 
-  // Check if the testRailIdMatch is not included in testPurposesTypes
-  if (
-    testRailIdMatch &&
-    !testPurposesTypes.includes(testRailIdMatch[0].replace(/[[\]]/g, ''))
-  ) {
-    const testRailId = testRailIdMatch[0].replace(/[[\]]/g, '');
-    return testRailId;
-  }
-
-  return null;
+  const manualTestIdMatch = test.title.match(/\[[a-zA-Z#-]+[0-9][^\]]*\]/);
+  return manualTestIdMatch ? manualTestIdMatch[0].replace(/[[\]]/g, '') : null;
 };
 
 /**
  * Extracts the test target type (type) from a full file path string if available.
  *
  * @param {string} fullFile - The full file path.
- * @param {Array<string>} testTargetsAvailable - Available test target types.
+ * @param {Array<string>} testTypesAvailable - Available test target types.
  * @returns {string} - The extracted type or an empty string if not found.
  * @throws {TypeError} If the provided arguments are not of expected types.
  */
-export const extractTypeFromFullFile = (fullFile, testTargetsAvailable) => {
+export const extractTypeFromFullFile = (fullFile, testTypesAvailable) => {
   if (typeof fullFile !== 'string') {
     throw new TypeError('The "fullFile" argument must be a string.');
   }
 
-  if (!Array.isArray(testTargetsAvailable)) {
+  if (!Array.isArray(testTypesAvailable)) {
     throw new TypeError(
-      'The "testTargetsAvailable" argument must be an array of strings.'
+      'The "testTypesAvailable" argument must be an array of strings.'
     );
   }
 
   const type =
-    testTargetsAvailable.find((targetType) =>
+    testTypesAvailable.find((targetType) =>
       fullFile.includes(`${targetType}/`)
     ) || '';
 
